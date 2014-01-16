4f7fc13e62947bd570394af7ea65613029f03370
Mark Woo Yong Ming <wooym2@skinner.cs.byu.edu>
2014-01-16 12:03:58 -0700

    changing config

--- config.sh
+++ config.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-sudo yum -y update
-sudo yum install -y gcc make
-sudo yum -y install httpd
-sudo service httpd start
-
-sudo /sbin/chkconfig --levels 235 httpd on
-sudo service httpd start
-
-echo "<html><body>mark woo</body></html>" > var/www/index.html
-
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
