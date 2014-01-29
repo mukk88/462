00e1bc94225d3a216d73552ba94537eb5a2d6964
Mark Woo Yong Ming <wooym2@hiking.cs.byu.edu>
2014-01-29 13:17:25 -0700

    express

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('../key.pem'),
+  cert: fs.readFileSync('../cert.pem')
+};
+
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -22,6 +30,7 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', require('ejs').renderFile);
 
 // development only
 if ('development' == app.get('env')) {
@@ -31,6 +40,8 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(8080, function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+https.createServer(options, app).listen(443);
