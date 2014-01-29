7c10ed5a1a4c14dad8bccd7906310c55aa1a520b
Mark Woo Yong Ming <wooym2@hiking.cs.byu.edu>
2014-01-29 12:22:44 -0700

    updating app

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var http = require('http');
 var app = express();
 
 app.get('/', function(req, res){
   res.send('Hello World');
 });
 
-app.listen(3000);
+
+var server = http.createServer(app);
+server.listen(3000, function () {
+  console.log('Express server listening on port 3000';
+});
