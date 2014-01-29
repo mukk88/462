d33cd871e15f151b2e1a7966e266c49b695af245
Mark Woo Yong Ming <wooym2@hiking.cs.byu.edu>
2014-01-29 13:09:23 -0700

    renaming app file

---
+++ app2.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+var app = express();
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+
+http.createServer(app).listen(8080);
+https.createServer(options, app).listen(443);
