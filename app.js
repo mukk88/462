9fddb32220e5ed846dfad6c82beb94897c33e255
Mark Woo Yong Ming <wooym2@skinner.cs.byu.edu>
2014-01-30 11:18:11 -0700

    middleware

--- app.js
+++ app.js
@@ -56,21 +56,24 @@ var app = express();
 
 // all environments
 // app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.session({ secret: 'keyboard cat' }));
-app.use(passport.session());
-app.use(passport.initialize());
-app.use(app.router);
-app.engine('html', require('ejs').renderFile);
+app.configure(function(){
+	app.set('views', path.join(__dirname, 'views'));
+	// app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded());
+
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.session({ secret: 'keyboard cat' }));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.engine('html', require('ejs').renderFile);
+});
 
 // development only
 if ('development' == app.get('env')) {
