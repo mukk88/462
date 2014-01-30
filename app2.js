63112730c39df364d7102fd463f25b792b81b517
Mark Woo Yong Ming <wooym2@skinner.cs.byu.edu>
2014-01-30 11:45:18 -0700

    app2

--- app2.js
+++ app2.js
@@ -4,8 +4,8 @@ var https = require('https');
 var fs = require('fs');
 
 var options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync('../key.pem'),
+  cert: fs.readFileSync('../cert.pem')
 };
 
 var app = express();
