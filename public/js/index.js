bbb61a30f65064e4c48b974090674650a4cf9bf4
Mark Woo Yong Ming <wooym2@skinner.cs.byu.edu>
2014-01-30 12:38:32 -0700

    checking user

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,4 @@ $(document).ready(function() {
 		window.location.href = '/account';
 	// 	window.location.href = "https://foursquare.com/oauth2/authenticate?client_id=ADKJ24OB2LPM0DZ1Y3X1VM53FRNKYLGT4W0PISMZCQNJQ1NG&response_type=code&redirect_uri=http://ec2-54-242-119-246.compute-1.amazonaws.com";
 	});
-	
-	console.log(user);
-
 });
