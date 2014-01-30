e146bdc8df880d9386e63f8e952f7397781b542c
Mark Woo Yong Ming <wooym2@skinner.cs.byu.edu>
2014-01-30 11:14:55 -0700

    middleware

--- app.js
+++ app.js
@@ -63,10 +63,13 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.session({ secret: 'keyboard cat' }));
 app.use(passport.session());
 app.use(passport.initialize());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 app.engine('html', require('ejs').renderFile);
 
 // development only
