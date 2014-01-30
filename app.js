22d506bf664cef11400f219549a8f3a7b1dc1034
Mark Woo Yong Ming <wooym2@skinner.cs.byu.edu>
2014-01-30 11:11:27 -0700

    typo

--- app.js
+++ app.js
@@ -63,9 +63,9 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
-app.use(passport.initialize());
 app.use(passport.session());
+app.use(passport.initialize());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('html', require('ejs').renderFile);
 
